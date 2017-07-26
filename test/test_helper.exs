3642991c512dc7afd118ceb995808b3c994ec5ef
Keith Brings <keith.brings@noizu.com>
2017-07-26 14:08:21 -0700

    empty gh-pages branch

--- test/test_helper.exs
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Author: Keith Brings
-# Copyright (C) 2017 Noizu Labs, Inc. All rights reserved.
-#-------------------------------------------------------------------------------
-
-ExUnit.start()
-
-Application.ensure_all_started(:bypass)
