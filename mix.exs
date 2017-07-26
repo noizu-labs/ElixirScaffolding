35a35f0816bfdee966b0e7de41768cc98ada8b00
Keith Brings <keith.brings@noizu.com>
2017-07-26 14:18:52 -0700

    Updating ex_doc version

--- mix.exs
+++ mix.exs
@@ -26,9 +26,9 @@ defmodule Noizu.Scaffolding.Mixfile do
 
   defp deps do
     [
-      { :amnesia, git: "https://github.com/meh/amnesia.git", ref: "87d8b4f", optional: true}, # Mnesia Wrapper
-      { :uuid, "~> 1.1" },
-      { :ex_doc, "~> 0.11", only: [:dev], optional: true },
+      {:amnesia, git: "https://github.com/meh/amnesia.git", ref: "87d8b4f", optional: true}, # Mnesia Wrapper
+      {:uuid, "~> 1.1" },
+      {:ex_doc, "~> 0.16.2", only: [:dev], optional: true}, # Documentation Provider
       {:markdown, github: "devinus/markdown", only: [:dev], optional: true}, # Markdown processor for ex_doc
     ]
   end # end deps
@@ -36,7 +36,7 @@ defmodule Noizu.Scaffolding.Mixfile do
   defp docs do
     [
       source_url_pattern: "https://github.com/noizu/ElixirScaffolding/blob/master/%{path}#L%{line}",
-      extras: ["README.md"]
+      extras: []
     ]
   end # end docs
 
